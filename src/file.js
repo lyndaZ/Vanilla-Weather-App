fa6f55f0caf557ce2f784105b33312b32614e860
lyndaZ <98845678+lyndaZ@users.noreply.github.com>
2023-06-03 03:21:04 +0200

    Changed date and time settings

--- src/file.js
+++ src/file.js
@@ -1,3 +1,20 @@
+function formatDate(timestamp){
+ let date = new date(timestamp);
+ let hours = date.getHours();
+ if (hours<10){
+    hours = `0${hours}`;
+ }
+ let minutes= date.getMinutes();
+ if (minutes<10){
+    minutes = `0${minutes}`;
+ }
+ let days = ["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"];
+ let day = days[date.getDay()];
+
+ return`${day} ${hours}:${minutes}`;
+}
+
+
 function displayTemperature(response){
     
     let temperatureElement = document.querySelector("#temperature");
@@ -10,17 +27,15 @@ function displayTemperature(response){
     humidityElement.innerHTML=response.data.main.humidity;
     let windElement=document.querySelector("#speed");
     windElement.innerHTML=Math.round(response.data.wind.speed);
-
-    
+    let dateElement=document.querySelector("#date");
+    dateElement.innerHTML= formatDate(response.data.dt * 1000);
 
 }
 
 
-
-
-
+let city = "Harare"
 let apiKey = "07a2b706ed187d0dc63c8d4b280d8b2c";
-let apiUrl= `https://api.openweathermap.org/data/2.5/weather?q=Johannesburg&appid=07a2b706ed187d0dc63c8d4b280d8b2c&units=metric`;
+let apiUrl= `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=07a2b706ed187d0dc63c8d4b280d8b2c&units=metric`;
 
 
 
