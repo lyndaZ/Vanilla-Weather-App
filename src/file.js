21ce8a3fd2dc69322ce1e996e73d70b07e2a84b4
lyndaZ <98845678+lyndaZ@users.noreply.github.com>
2023-06-15 23:37:49 +0200

    Revert "Dieplay forecast"
    This reverts commit d719f5d76e9f8b76ec08b7498023a55968be00c6.

--- src/file.js
+++ src/file.js
@@ -25,8 +25,8 @@ function formatDay(timestamp){
 }
 
 function displayForecast(response) {
-
-  let forecast =response.data.daily;
+   
+  let forecast = response.data.daily;
 
   let forecastElement = document.querySelector("#forecast");
 
@@ -62,7 +62,7 @@ function displayForecast(response) {
 function getForecast(coordinates){
     console.log(coordinates);
     
-    let apiKey = "07a2b706ed187d0dc63c8d4b280d8b2c";
+    let apiKey = "3bc520cc14bbdedfd7e45158f2ef0439";
     let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
 
   axios.get(apiUrl).then(displayForecast);
